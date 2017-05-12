87dc71077cda36cf3120f5b0297220ede04321ec
thejoin95 <miki.lombi@gmail.com>
2017-05-12 22:03:35 +0200

    searching for all parameters and requests

--- run.py
+++ run.py
@@ -43,12 +43,48 @@ def doLogin():
 
     loginPage_bs = BeautifulSoup(loginPage.text, 'html.parser')
     configValues['paramsLogin']['authenticity_token'] = loginPage_bs.find('meta', {'name': 'csrf-token'}).get('content')
-    csrfHeaders['X-CSRF-Token'] = configValues['paramsLogin']['authenticity_token']
+    configValues["csrfHeaders"]['X-CSRF-Token'] = configValues['paramsLogin']['authenticity_token']
 
     userLogin = makeRequest('POST', 'https://api.500px.com/v1/session', data = configValues['paramsLogin'])
     responseLoginPost = json.loads(userLogin.content)
+
+    # check for user data
+    # else search for this: window.PxCurrentUser.user and try to parse
+
     return (responseLoginPost['success'] == True and responseLoginPost['success'])
 
+def doLogout():
+    global userSession, configValues
+
+    # need to get back, from the last request, the "authenticity_token" of logout's form
+
+    logoutParams = {
+        "_method": "delete",
+        "authenticity_token": configValues["csrfHeaders"]['X-CSRF-Token']
+    }
+
+    logoutPage = makeRequest("POST", "https://500px.com/logout", data = logoutParams)
+    print logoutPage.status_code
+
+def unFollow():
+    # https://500px.com/{username}/unfollow POST
+
+def doFollow():
+    # https://500px.com/{username}/follow POST
+
+def doLike():
+     # https://api.500px.com/v1/photos/{idphoto}/vote?vote=1 POST
+
+def unLike():
+    # https://api.500px.com/v1/photos/{idphoto}/vote DELETE
+
+def getLikesOfPhoto():
+    # https://api.500px.com/v1/photos/{idphoto}/votes?include_following=true&page=2&rpp=8 GET
+
+def doComment():
+    # https://api.500px.com/v1/photos/211674051/comments?sort=created_at&include_subscription=1&include_flagged=1&nested=1 POST
+    # payload JSON, need to GET all profile data :S
+    # {"body":"Really nice shot! love the combination and the colors :)","user":{"id":6775294,"username":"mikistorm","firstname":"Miki","lastname":"Lombardi","fullname":"Miki Lombardi","affection":1310,"userpic_url":"https://pacdn.500px.org/6775294/2b0940d604a671bd4a8ddb2d8ee2c81920d13d75/1.jpg?1","userpic_https_url":"https://pacdn.500px.org/6775294/2b0940d604a671bd4a8ddb2d8ee2c81920d13d75/1.jpg?1","cover_url":"https://pacdn.500px.org/6775294/2b0940d604a671bd4a8ddb2d8ee2c81920d13d75/cover_2048.jpg?4","upgrade_status":0,"usertype":0,"city":"Firenze","state":"","country":"Italy","admin":false,"avatars":{"default":{"https":"https://pacdn.500px.org/6775294/2b0940d604a671bd4a8ddb2d8ee2c81920d13d75/1.jpg?1"},"large":{"https":"https://pacdn.500px.org/6775294/2b0940d604a671bd4a8ddb2d8ee2c81920d13d75/2.jpg?1"},"small":{"https":"https://pacdn.500px.org/6775294/2b0940d604a671bd4a8ddb2d8ee2c81920d13d75/3.jpg?1"},"tiny":{"https":"https://pacdn.500px.org/6775294/2b0940d604a671bd4a8ddb2d8ee2c81920d13d75/4.jpg?1"}},"show_groups_onboarding":false,"followers_count":11,"photos_count":131,"views_count":19225,"followees_count":98,"email":"mikistorm@live.it","upload_limit":20,"store_enabled":true,"show_nude":false,"registration_date":"2014-02-03T04:21:46-05:00","birthday":"1995-06-15","upgrade_expiry_date":"2015-06-23","avatar_version":1,"needs_contact_verification":false,"buyer":false,"photo_availability_filter":1},"replies":[]}
 
 if __name__ == '__main__':
 
@@ -65,12 +101,11 @@ if __name__ == '__main__':
             'authenticity_token': '',
             'session[email]': '',
             'session[password]': ''
+        },
+        "csrfHeaders": {
+            'X-CSRF-Token': '',
+            'X-Requested-With': 'XMLHttpRequest'
         }
-    }
-
-    csrfHeaders = {
-        'X-CSRF-Token': '',
-        'X-Requested-With': 'XMLHttpRequest'
     }
 
     try:
